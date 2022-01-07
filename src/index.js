74657d50978698de557f003f8a5a41fb8e176269
Kapil Kandel <kandelkapil01@gmail.com>
2022-01-07 11:04:39 +0545

    wrapped app with provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import ReactDom from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 ReactDom.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
 
   document.getElementById("root")
